9a4871bff5c314fc45c23497976170b1f0c2ac0a
briansastre-ops <briansastre159@gmail.com>
2024-06-04 17:53:00 -0300

    SideBar Home

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,16 @@
+import SideBar from "@/components/SideBar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {firstName:'Brian', lastName:'Sastre'}
+
   return (
-    <main>
-        sidebar
+    <main className="flex h-screen w-full font-inter">
+        <SideBar user={loggedIn}/>
         {children}
     </main>
   );
