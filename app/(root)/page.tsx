5d2f279de231e20e828590e21b3023a92942749d
briansastre-ops <briansastre159@gmail.com>
2024-06-19 10:20:04 -0300

    RightSidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstName: 'Brian'}
+  const loggedIn = {firstName: 'Brian', lastName: 'Sastre', email: 'Briansatre159@gmail.com'}
 
   return (
     <section className='home'>
@@ -22,7 +23,14 @@ const Home = () => {
             totalCurrentBalance= {1250.54}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user= {loggedIn}
+        transactions= {[]}
+        banks ={[]}
+      />
     </section>
   )
 }
