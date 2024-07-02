fbf589534516d5380ff42ff896e12d5ff7bc5b51
briansastre-ops <briansastre159@gmail.com>
2024-07-02 10:38:21 -0300

    Auth Form

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 /*import { z } from "zod"; */
 
 export function cn(...inputs: ClassValue[]) {
@@ -209,3 +210,5 @@ export const authFormSchema = (type: string) => z.object({
   email: z.string().email(),
   password: z.string().min(8),
 })
+
+
